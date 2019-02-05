77707d0c9f924ac6ca0c9645cd68dee4d5aa63fb
César Seabra <cesar.seabra.95@gmail.com>
2019-02-05 01:20:54 +0000

    Changes to the UI

---
+++ js/prototypes.js
@@ -0,0 +1,7 @@
+var Proto = {
+
+    urlInput: '<div class="input-section"><input class="url-input"><button class="url-delete">Delete</i></button></div>',
+    errorMessage: '<div class="message error">{{$message}}</div>',
+    successMessage: '<div class="message success">{{$message}}</div>',
+    warningMessage: '<div class="message warning">{{$message}}</div>',
+}
